e5eb167aaa0e2009255357017625188c7ba8f261
iii <blbaileyiii.naa@gmail.com>
2021-03-14 10:23:31 -0700

    a

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .use('/db', dbRouter)
-  .use('/eowdb', eowDBRouter)
-  .use('/getPerson', personRouter)
-  .use('/postalrate', getRateRouter)
+  .use('/db', dbRouter) //Homework
+  .use('/getPerson', personRouter) //Homework
+  .use('/postalrate', getRateRouter) //Homework
+  .use('/eowdb', eowDBRouter) // Project 2
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
